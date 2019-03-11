ef5b22c3d6b98ccd98c5109893bc91988de1609f
Andre Carter <acarter@cartersolutions.com>
2019-03-11 15:59:00 -0400

    adding files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FaqComponent } from './faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'faq', component: FaqComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
